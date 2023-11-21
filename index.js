ce96938f8779d435851dceadbe95d27263af1bd2
firatakkoc <firat.akkoc@outlook.com>
2023-11-21 18:20:49 +0100

    Template Engine: Ejs

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+
 const path = require("path");
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,56 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+
+const data = {
+  title: "Beliebte Schulungen",
+  categories: ["Web Development", "Programming", "Mobil Applications", "Data Analys"],
+  blogs: [
+    {
+      blogid: 1,
+      title: "baslik1",
+      des: "aciklama",
+      img: "https://images.pexels.com/photos/4308095/pexels-photo-4308095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      homepage: true,
+      onay: true
+    },
+    {
+      blogid: 2,
+      title: "baslik2",
+      des: "aciklama",
+      img: "https://images.pexels.com/photos/5940721/pexels-photo-5940721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      homepage: true,
+      onay: true
+    },
+    {
+      blogid: 3,
+      title: "baslik3",
+      des: "aciklama",
+      img: "https://images.pexels.com/photos/7516381/pexels-photo-7516381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      homepage: true,
+      onay: false
+    },
+    {
+      blogid: 4,
+      title: "baslik4",
+      des: "aciklama",
+      img: "https://images.pexels.com/photos/1325659/pexels-photo-1325659.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      homepage: true,
+      onay: false
+    },
+  ]
+};
 
 router.use("/blogs/:blogid",function(req, res){
 
-  res.sendFile(path.join(__dirname, "../views/users","blog-details.html"));
+  res.render("users/blog-details");
 });
 
 router.use("/blogs",function(req, res){
-  res.sendFile(path.join(__dirname, "../views/users","blogs.html"));
+  res.render("users/blogs", data);
 });
 
 router.use("/",function(req, res){
-  res.sendFile(path.join(__dirname, "../views/users","index.html"));
+  res.render("users/index", data);
 });
 
 module.exports = router;
